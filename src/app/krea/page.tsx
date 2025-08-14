e28997ef42473c8b29a08bd79689434a7584e71e
rquanx <runquant@gmail.com>
2025-08-15 03:28:46 +0800

    fix: build error

--- src/app/krea/page.tsx
+++ src/app/krea/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import Editor from "../components/Editor";
 
 export default function Krea() {
-  return <Editor />
+  return <Suspense>
+    <Editor /></Suspense>
 }
